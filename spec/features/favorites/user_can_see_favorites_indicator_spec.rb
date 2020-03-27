f357970b51fcb092c9054496c28779ef843416b0
Ana Perez Santos <a.jperez@yahoo.com>
2020-03-27 16:14:21 -0600

    US 11: Create Favorites link in the nvigation bar

--- spec/features/favorites/user_can_see_favorites_indicator_spec.rb
+++ spec/features/favorites/user_can_see_favorites_indicator_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Favorites indicator', type: :feature do
+RSpec.describe 'as a user I see a favorites indicator', type: :feature do
   it "shows number of favorite pets in the navigation bar from any page" do
 
     visit '/shelters'
@@ -11,4 +11,22 @@ RSpec.describe 'Favorites indicator', type: :feature do
 
     expect(page).to have_content("Favorites: 0")
   end
+
+  it "links to the favorites index page when I click on it" do
+    visit '/shelters'
+
+    click_link "Favorites: 0"
+    expect(current_path).to eq("/favorites")
+
+    visit '/pets'
+
+    click_link "Favorites: 0"
+    expect(current_path).to eq("/favorites")
+  end
 end
+
+  # User Story 11, Favorite Indicator links to Index Page
+  #
+  # As a visitor
+  # When I click on the favorite indicator in the nav bar
+  # I am taken to the favorites index page
