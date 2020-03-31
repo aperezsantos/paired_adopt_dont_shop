a71e1a70bfc488f4ddff5c3fee6b325d7fcbb7e8
Fred Rondina <fredrondina96@gmail.com>
2020-03-31 15:58:22 -0600

    Completed User story 30 plus tests

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -14,4 +14,30 @@ RSpec.describe Shelter, type: :model do
       it {should have_many :reviews}
   end
 
+  describe "#application_count" do
+    it "returns the number of applications for the shelter" do
+      shelter1 = Shelter.create!(name: "Larry's Lizards", address: "1331 17th Street", city: 'Denver', state: 'CO', zip: "80202")
+      pet1 = shelter1.pets.create!(name: "Sam", age: "12", sex: "Female", image: "https://66.media.tumblr.com/6a9b0ea4859319c0defd9681b3a78e8f/tumblr_n8o33kXRnG1qhaglio1_r1_1280.png")
+      pet2 = shelter1.pets.create!(name: "Melo", age: "3", sex: "Male", image: "https://i.pinimg.com/474x/8f/c4/68/8fc46860f9f52463e4e9db1ec32044f4--hady-reptile-room.jpg")
+
+      application1 = Application.create!(name: "Jimbo", address: "test address", city: "test city", state: "test state", zip: "test zipcode", phone_number: "test phone number", description: "test description")
+      application1.pets << pet1
+      application1.pets << pet2
+
+      expect(shelter1.application_count).to eq(1)
+    end
+  end
+
+  describe "#average_review_rating" do
+    it "returns the average review rating for a shelter" do
+      shelter1 = Shelter.create!(name: "Larry's Lizards", address: "1331 17th Street", city: 'Denver', state: 'CO', zip: "80202")
+      expect(shelter1.average_review_rating).to eq(0)
+
+      review1 = shelter1.reviews.create!(title: "This place is great", rating: "5", content:"wow good lizards", image: "https://c7.alamy.com/comp/XAABRH/guy-with-thumbs-up-XAABRH.jpg")
+      review2 = shelter1.reviews.create!(title: "Only lizards wtf", rating: "1", content:"I wanted a dog", image: "https://c7.alamy.com/comp/XAABRH/guy-with-thumbs-up-XAABRH.jpg")
+
+      expect(shelter1.average_review_rating).to eq(3)
+    end
+  end
+
 end
